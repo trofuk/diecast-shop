26f50dc43238558ffa704599e46d53a709e82a00
Dmytro Trofimenko <trofuk@gmail.com>
2016-06-12 17:47:50 +0300

    Diecast shop

---
+++ index.php
@@ -0,0 +1,369 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<title>Diecast Hobby Shop</title>
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="/css/normalize.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.1.5/assets/owl.carousel.min.css">
+		<link href="/css/style.css" rel="stylesheet">
+		<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,400italic,700,700italic&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+		<link href='https://fonts.googleapis.com/css?family=EB+Garamond' rel='stylesheet' type='text/css'>
+		<link href='https://fonts.googleapis.com/css?family=Bentham' rel='stylesheet' type='text/css'>
+		
+	</head>
+	
+	
+	
+	
+	<body>
+		<div class="header"> 
+			<div class="header-content">
+				<div class="logo">
+					<a href="">
+						<img class="logo-pic" src="images/logo.png" alt="">	
+					</a>	
+				</div>
+				<!--Верхня менюшка-->
+				<div class="site-nav">
+					<div class="buttons">
+						<input type="submit" id="subscribe_send" class="distr_but" value="про магазин">
+						<input type="submit" id="subscribe_send" class="distr_but" value="оплата/доставка">
+						<input type="submit" id="subscribe_send" class="distr_but" value="гарантії">
+						<input type="submit" id="link" class="distr_but" value="посилання">
+						<input type="submit" id="subscribe_send" class="distr_but" value="контакти">
+					</div>
+				</div>
+				<!--Нижня менюшка-->
+				<div class="site-nav-2">
+					<ul>
+						<li><a href="">Акції</a></li>
+						<li><a href="">Каталог</a></li>
+						<li><a href="">Статті</a></li>
+						<li><a href="">Форум</a></li>
+						
+					</ul>
+				</div>
+				
+			</div>
+			
+		</div>
+		
+		
+		<div class="main">
+			<!--Лівий блок-->
+			<article class="leftBlock">
+				<!--Вхід в кабінет-->
+				<div class="ltit"><p>мій кабінет</p></div>
+				<div class="distributionBlock">
+					<form action="" method="">
+						<input type="text" id="enter_login" name="FORM[login]" class="distr_input" placeholder="Введіть свій логін">
+						<input type="submit" id="subscribe_send" class="distr_but" value="увійти">
+					</form>
+				</div>
+				<!--Пошук-->
+				<div class="ltit"><p>пошук моделей</p></div>
+				<div class="distributionBlock">
+					<form action="" method="">
+						<input type="text" id="search" name="FORM[search]" class="distr_input" placeholder="Пошук...">
+						<input type="submit" id="subscribe_send" class="distr_but" value="шукати">
+					</form>
+				</div>
+				<!--Кошик з купленими товарами-->
+				<div class="ltit"><p>кошик</p></div>
+				<div class="distributionBlock">
+					<form action="" method="">
+						<input type="text" id="shopping_cart" name="FORM[cart]" class="distr_input" placeholder="Кошик порожній">
+						<input type="submit" id="subscribe_send" class="distr_but" value="переглянути">
+					</form>
+				</div>
+				
+				
+				<!--Блок новин-->
+				<div class="ltit"><p>новини</p></div>
+				<div class="newsContent">
+					<div class="newsBlock">
+						<div class="n_head"><a class="n_name" href="" style="font-size: 16px;">ВАЗ 2101 "Жигули" 1971</a></div>
+						<div class="date">19-09-2015</div>
+						<a href="">Кітайська компанія "IST Models"...</a>
+					</div>
+					<div class="newsBlock">
+						<div class="n_head"><a class="n_name" href="" style="font-size: 16px;">BMW X3</a></div>
+						<div class="date">19-09-2015</div>
+						<a href="">До продажу надійшли бюджетні...</a>
+					</div>
+					<div class="newsBlock">
+						<div class="n_head"><a class="n_name" href="" style="font-size: 16px;">Toyota RAV4 (SUV) 2009</a></div>
+						<div class="date">19-09-2015</div>
+						<a href="">До продажу надійшла колекційна...</a>
+					</div>
+					<div class="tar" style="text-align: center;"><a href="">Переглянути всі новини</a></div>
+				</div>
+				
+			</article>
+			
+			
+			
+			<!--Правий блок-->
+			<div class="rightBlock">
+				<div class="ctit"><p>нові надходження</p></div>
+				
+				<!--Перший слайдер-->
+				<div class="carousel">
+					<div class="carousel_wrapper">
+						<div class="owl-carousel">
+							
+							<div class="box">
+								<div class="boxImg">
+									<a href=""></a>
+									<img src="images/476_1.jpg" alt="ПМЗ 27 (157К) пожарный (SSM)">
+								</div>
+								<a href="" class="tName">ПМЗ 27 (157К) пожежний (SSM)</a>
+								<div class="price">
+									<!--<p>0 грн.</p>-->
+									<a href="" class="buyBtn green_btn" id="476">купити</a>
+								</div>
+							</div>
+							
+							<div class="box">
+								<div class="boxImg">
+									<a href=""></a>
+									<img src="images/473_1.jpg" alt="Напівпричіп МАЗ 5215 блакитний (SSM)">
+								</div>
+								<a href="" class="tName">Напівпричіп МАЗ 5215 блакитний (SSM)</a>
+								<div class="price">
+									<!--<p>0 грн.</p>-->
+									<a href="" class="buyBtn green_btn" id="473">купити</a>
+								</div>							
+							</div>
+							
+							<div class="box">
+								<div class="boxImg">
+									<a href=""></a>
+									<img src="images/479_1.jpg" alt="АЦ 40 (131)-137, без написів (SSM)">
+								</div>
+								<a href="" class="tName">АЦ 40 (131)-137, без написів (SSM)</a>
+								<div class="price">
+									<!--<p>0 грн.</p>-->
+									<a href="" class="buyBtn green_btn" id="479">купити</a>
+								</div>								
+							</div>
+							
+							<div class="box">
+								<div class="boxImg">
+									<a href=""></a>
+									<img src="images/486_1.jpg" alt="Автокран КС 3575А (133ГЯ) синій/жовтий (SSM)">
+								</div>
+								<a href="" class="tName">Автокран КС 3575А (133ГЯ) синій/жовтий (SSM)</a>
+								<div class="price">
+									<!--<p>0 грн.</p>-->
+									<a href="" class="buyBtn green_btn" id="486">купити</a>
+								</div>
+							</div>
+							
+							<div class="box">
+								<div class="boxImg">
+									<a href=""></a>
+									<img src="images/474_1.jpg" alt="Автокран КС 3575А (133ГЯ) аварійний (SSM)">
+								</div>
+								<a href="" class="tName">Автокран КС 3575А (133ГЯ) аварійний (SSM)</a>
+								<div class="price">
+									<!--<p>0 грн.</p>-->
+									<a href="" class="buyBtn green_btn" id="474">купити</a>
+								</div>
+							</div>
+							
+							<div class="box">								
+								<div class="boxImg">
+									<a href=""></a>
+									<img src="images/472_1.jpg" alt="Напівпричіп МАЗ 5215 помаранчевий (SSM)">
+								</div>
+								<a href="" class="tName">Напівпричіп МАЗ 5215 помаранчевий (SSM)</a>
+								<div class="price">
+									<!--<p>0 грн.</p>-->
+									<a href="" class="buyBtn green_btn" id="472">купити</a>
+								</div>
+							</div>
+							
+							<div class="box">
+								<div class="boxImg">
+									<a href=""></a>
+									<img src="images/478_1.jpg" alt="ПМЗ 27А (157К) ДПД колгоспу ім. Дзержинського (SSM)">
+								</div>
+								<a href="" class="tName">ПМЗ 27А (157К) ДПД колгоспу ім. Дзержинського (SSM)</a>
+								<div class="price">
+									<!--<p>0 грн.</p>-->
+									<a href="" class="buyBtn green_btn" id="478">купити</a>
+								</div>
+							</div>
+							
+							<div class="box">
+								<div class="boxImg">
+									<a href=""></a>
+									<img src="images/477_1.jpg" alt="АЦ 40 (131)-137 УПЧ м. Кострома (SSM)">
+								</div>
+								<a href="" class="tName">АЦ 40 (131)-137 УПЧ м. Кострома (SSM)</a>
+								<div class="price">
+									<!--<p>0 грн.</p>-->
+									<a href="" class="buyBtn green_btn" id="477">купити</a>
+								</div>
+							</div>
+							
+							<div class="box">
+								<div class="boxImg">
+									<a href=""></a>
+									<img src="images/475_1.jpg" alt="Автокран КС 3575А (133ГЯ) хакі/сірий (SSM)">
+								</div>
+								<a href="" class="tName">Автокран КС 3575А (133ГЯ) хакі/сірий (SSM)</a>
+								<div class="price">
+									<!--<p>0 грн.</p>-->
+									<a href="" class="buyBtn green_btn" id="475">купити</a>
+								</div>
+							</div>
+							
+							<div class="box">
+								<div class="boxImg">
+									<a href=""></a>
+									<img src="images/481_1.jpg" alt="ПМЗ 27 (157К) м. Томськ, ПЧ №7 (SSM)">
+								</div>
+								<a href="" class="tName">ПМЗ 27 (157К) м. Томськ, ПЧ №7 (SSM)</a>
+								<div class="price">
+									<!--<p>0 грн.</p>-->
+									<a href="" class="buyBtn green_btn" id="481">купити</a>
+								</div>
+							</div>
+							
+							<div class="box">
+								<div class="boxImg">
+									<a href=""></a>
+									<img src="images/480_1.jpg" alt="ПМЗ 27 (157К) білі смуги, без написів (SSM)">
+								</div>
+								<a href="" class="tName">ПМЗ 27 (157К) білі смуги без написів (SSM)</a>
+								<div class="price">
+									<!--<p>0 грн.</p>-->
+									<a href="" class="buyBtn green_btn" id="480">купити</a>
+								</div>
+							</div>
+							
+						</div>
+					</div>
+				</div>
+								
+				<div class="ctit"><p>уцінені товари</p></div>
+				<!--Другий слайдер-->
+				<div class="carousel">
+					<div class="carousel_wrapper">
+						<div class="owl-carousel">
+							
+							<div class="box">
+								<div class="boxImg">
+									<a href=""></a>
+									<img src="images/483_1.jpg" alt="Коток ДУ 49 жовтий (SSM)">
+								</div>
+								<a href="" class="tName">Коток ДУ 49 жовтий (SSM)</a>
+								<div class="price">
+									<!--<p>0 грн.</p>-->
+									<a href="" class="buyBtn green_btn" id="483">купити</a>
+								</div>
+							</div>
+							
+							<div class="box">
+								<div class="boxImg">
+									<a href=""></a>
+									<img src="images/485_1.jpg" alt="ЛіАЗ 5256 міський білий/червоний (SSM)">
+								</div>
+								<a href="" class="tName">ЛіАЗ 5256 міський білий/червоний (SSM)</a>
+								<div class="price">
+									<!--<p>0 грн.</p>-->
+									<a href="" class="buyBtn green_btn" id="485">купити</a>
+								</div>
+							</div>
+							
+							<div class="box">
+								<div class="boxImg">
+									<a href=""></a>
+									<img src="images/482_1.jpg" alt="Коток ДУ 49 блідо-жовтий (SSM)">
+								</div>
+								<a href="" class="tName">Коток ДУ 49 блідо-жовтий (SSM)</a>
+								<div class="price">
+									<!--<p>0 грн.</p>-->
+									<a href="" class="buyBtn green_btn" id="482">купити</a>
+								</div>
+							</div>
+							
+							<div class="box">
+								<div class="boxImg">
+									<a href="h"></a>
+									<img src="images/484_1.jpg" alt="ЛіАЗ 5256 міський білий/зелений (SSM)">
+								</div>
+								<a href="" class="tName">ЛіАЗ 5256 міський білий/зелений (SSM)</a>
+								<div class="price">
+									<!--<p>0 грн.</p>-->
+									<a href="" class="buyBtn green_btn" id="484">купити</a>
+								</div>
+							</div>
+							
+						</div>
+					</div>
+				</div>
+				<!--Статті-->
+				<article class="content" style="font-family: 'EB Garamond', serif; font-size: medium; color: #5c1e18; padding-left: 10px;">			            
+					<h1>Масштабні автомоделі (машини) в масштабі 1:43</h1>
+					<p><img src="images/212_1.jpg" border="0" alt="моделі автомобілів 1:43" title="моделі автомобілів 1:43" width="240" height="135" style="float: right; margin: 5px;">Наш інтернет-магазин колекційних масштабних моделей автомобілів пропонує на Ваш вибір велику колекцію моделей машин різних вітчизняних і зарубіжних виробників. Ми намагаємося запропонувати Вам в самому широкому асортименті такі моделі як: легкові, вантажні та службові автомобілі, спорткари, автобуси, мотоцикли, спецтехніка, військова техніка та інші.</p>
+					<h2>Масштабні моделі 1:43</h2>
+					<p><img src="images/121_1.jpg" border="0" alt="моделі автомобілів 1:43" title="моделі автомобілів 1:43" width="240" height="135" style="float: right; margin: 5px;">Важко собі уявити сучасне життя без автомобілів. При цьому все більше число людей проявляють інтерес не тільки до останніх новинок автомобільного ринку, але і до безпосередньо історії автомобілебудування, в тому числі і нашої країни. Що стосується появи автомобіля, то дякувати за це треба, перш за все, німецьких конструкторів Готтлиба Даймлера і Карда Бенца. В інших країнах епоху автомобілізації пов'язують з наступними іменами: у Франції Луї Пенар і Еміль Льовассор, в Англії Фредерік Ланчестер і Джон Найт, в США Френк Дюріе і Генрі Форд, в Італії Енріке Бернарди і Арістід Фачьоллі, в Росії Євген Яковлєв та Петро Фрезе. Історія автомобіля дуже велика і цікава тема, яка подарує багато позитивних емоцій при її вивченні. Саме тому автомобільні масштабні моделі в Україні так популярні. Варто зазначити, що особливою популярністю користуються колекційні моделі автомобілів в співвідношенні 1:43. Незважаючи на те, що існують і моделі з іншим співвідношенням, саме моделі автомобілів 1:43 мають найбільший попит. Це своєрідний еталон серед колекційних моделей, саме тому автомоделей 1:43 в Україні користуються найбільшою популярністю серед колекціонерів.</p>
+					<h2>Моделі 1:43: переваги перед іншими масштабами</h2>
+					<p><img src="images/205_1.jpg" border="0" alt="масштабні моделі 1:43 Україна" title="масштабні моделі 1:43 Україна" width="240" height="135" style="float: left; margin-left: 5px; margin-right: 5px;">Головною перевагою, яким володіють масштабні моделі 1:43 є невеликий розмір. Завдяки компактним габаритам модель не займе багато місця. Автомоделей 1:43 виготовляються зазвичай з металу або пластику, і мають високу ступінь опрацювання деталей. Якщо більш детально придивитися до масштабних моделей 1:43, то стає зрозуміло за рахунок чого вони стали такими популярними. Моделі в масштабі 1:43 займають менше місця на полиці, ніж, наприклад, моделі в масштабі 1:24. Крім того, в таких моделях максимальна деталізація: салон, скляні фари, колеса і т.д. У багатьох моделей можуть відкриватися капот, двері, багажник. Виробництвом масштабних моделей займається невелика кількість компаній, тому вони настільки популярні серед колекціонерів. Колекційні моделі 1:43 (Україна) представлені в дуже великій різноманітності: моделі цивільних автомобілів СРСР і зарубіжжя, військова техніка, громадський транспорт і багато іншого.</p>
+				</article>				
+			</div>
+			<div class="clear"></div>
+		</div>
+		<!--Скрипт слайдера-->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.1.5/owl.carousel.min.js"></script>
+		
+		<script>
+			$(document).ready(function(){
+				$('.carousel .owl-carousel').owlCarousel({
+					navText: ['',''],
+					loop:true,
+					smartSpeed:1000,
+					responsive:{
+						0: {
+							items: 2
+						},
+						768:{
+							items: 3,
+							nav: true,
+							slideBy: 1
+						}                  
+					}
+				});
+			});
+		</script>	
+		
+	</div>
+	
+	
+	<div class="footer">
+		
+		<div class="footer-content">
+		
+			<!--Менюшка футера-->
+			<div class="buttons">
+				<input type="submit" id="subscribe_send" class="distr_but" value="головна">
+				<input type="submit" id="subscribe_send" class="distr_but" value="оплата/доставка">
+				<input type="submit" id="subscribe_send" class="distr_but" value="про магазин">
+				<input type="submit" id="news" class="distr_but" value="новини">
+				<input type="submit" id="subscribe_send" class="distr_but" value="контакти">
+			</div>
+			<h4 class="copyright">Copyright 2016 / All rights reserved / Trofuk.com</h4>
+			<h4 class="designed">Designed by Trofuk.com</h4>
+		</div>
+	</div>
+	
+	
+</body>
+
+</html>						
